a54e25f7ecc04bf1ceca63a54ff64dc56d462c3c
Andrejus <andrejuszl@gmail.com>
2012-07-11 02:54:56 +0300

    Admin pagrindinis failas

---
+++ 1.5.3.1/admin/language/lithuanian/lithuanian.php
@@ -0,0 +1,114 @@
+<?php
+// Locale
+$_['code']                    = 'lt';
+$_['direction']               = 'ltr';
+$_['date_format_short']       = 'Y/m/d';
+$_['date_format_long']        = 'l dS F Y';
+$_['time_format']             = 'h:i:s A';
+$_['decimal_point']           = ',';
+$_['thousand_point']          = '';
+
+// Text
+$_['text_yes']                = 'Taip';
+$_['text_no']                 = 'Ne';
+$_['text_enabled']            = 'Aktyvuota';
+$_['text_disabled']           = 'Deaktyvuota';
+$_['text_none']               = ' --- Nėra --- ';
+$_['text_select']             = ' --- Pasirinkite --- ';
+$_['text_select_all']         = 'Pažymėti viską';
+$_['text_unselect_all']       = 'Atžymėti viską';
+$_['text_all_zones']          = 'Visos zonos';
+$_['text_default']            = ' <b>(Numatytasis)</b>';
+$_['text_close']              = 'Uždaryti';
+$_['text_pagination']         = 'Rodoma {start} iki {end} iš {total} ({pages} psl.)';
+$_['text_no_results']         = 'Nieko nerasta!';
+$_['text_separator']          = ' &gt; ';
+$_['text_edit']               = 'Redaguoti';
+$_['text_view']               = 'Žiūrėti';
+$_['text_home']               = 'Pradžia';
+
+// Button
+$_['button_insert']           = 'Pridėti';
+$_['button_delete']           = 'Ištrinti';
+$_['button_save']             = 'Išsaugoti';
+$_['button_cancel']           = 'Atšaukti';
+$_['button_clear']            = 'Išvalyti įrašus';
+$_['button_close']            = 'Uždaryti';
+$_['button_filter']           = 'Filtruoti';
+$_['button_send']             = 'Siųsti';
+$_['button_edit']             = 'Taisyti';
+$_['button_copy']             = 'Kopijuoti';
+$_['button_back']             = 'Atgal';
+$_['button_remove']           = 'Pašalinti';
+$_['button_backup']           = 'Atsarginė kopija';
+$_['button_restore']          = 'Atstatyti';
+$_['button_upload']           = 'Įkelti';
+$_['button_submit']           = 'Pateikti';
+$_['button_invoice']          = 'Spausdinti sąskaita-faktūrą';
+$_['button_add_address']      = 'Pridėti adresą';
+$_['button_add_attribute']    = 'Pridėti atributą';
+$_['button_add_banner']       = 'Pridėti reklamą';
+$_['button_add_product']      = 'Pridėti produktą';
+$_['button_add_voucher']      = 'Pridėti kuponą';
+$_['button_add_option']       = 'Pridėti pasirinkimą';
+$_['button_add_option_value'] = 'Pridėti pasirinkimo reikšmę';
+$_['button_add_discount']     = 'Pridėti nuolaidą';
+$_['button_add_special']      = 'Pridėti ypatingą pasiųlymą';
+$_['button_add_image']        = 'Pridėti atvaizdą';
+$_['button_add_geo_zone']     = 'Pridėti geografinę zoną';
+$_['button_add_history']      = 'Pridėti užsakymų istoriją';
+$_['button_add_transaction']  = 'Pridėti sandorį';
+$_['button_add_total']        = 'Pridėti sumą';
+$_['button_add_reward']       = 'Pridėti lojalumo taškus';
+$_['button_add_route']        = 'Pridėti maršrutą';
+$_['button_add_rule' ]        = 'Pridėti taisyklę';
+$_['button_add_module']       = 'Pridėti modulį';
+$_['button_add_link']         = 'Pridėti nuorodą';
+$_['button_update_total']     = 'Atnaujinti sumas';
+$_['button_approve']          = 'Patvirtinti';
+$_['button_reset']            = 'Išvalyti';
+
+// Tab
+$_['tab_address']             = 'Adresas';
+$_['tab_admin']               = 'Administratorius';
+$_['tab_attribute']           = 'Atributas';
+$_['tab_coupon_history']      = 'Kuponų istorija';
+$_['tab_customer']            = 'Kliento informacija';
+$_['tab_data']                = 'Duomenys';
+$_['tab_design']              = 'Dizainas';
+$_['tab_discount']            = 'Nuolaida';
+$_['tab_general']             = 'Bendrieji';
+$_['tab_fraud']               = 'Apgaulės';
+$_['tab_ip']                  = 'IP adresai';
+$_['tab_links']               = 'Nuorodos';
+$_['tab_image']               = 'Atvaizdas';
+$_['tab_option']              = 'Pasirinkimas';
+$_['tab_server']              = 'Serveris';
+$_['tab_store']               = 'Parduotuvė';
+$_['tab_special']             = 'Specialūs pasiūlymai';
+$_['tab_local']               = 'Vietovė';
+$_['tab_mail']                = 'Paštas';
+$_['tab_module']              = 'Modulis';
+$_['tab_order']               = 'Užsakymo detalės';
+$_['tab_order_history']       = 'Užsakymų istorija';
+$_['tab_payment']             = 'Mokėjimo detalės';
+$_['tab_product']             = 'Produktai';
+$_['tab_return']              = 'Grąžinimo detalės';
+$_['tab_return_history']      = 'Grąžinimų istorija';
+$_['tab_reward']              = 'Lojalumo taškai';
+$_['tab_shipping']            = 'Pristatymo detalės';
+$_['tab_total']               = 'Sumos';
+$_['tab_transaction']         = 'Sandoriai';
+$_['tab_voucher']             = 'Kuponai';
+$_['tab_voucher_history']     = 'Kuponų istorija';
+
+// Error
+$_['error_upload_1']          = 'Įspėjimas: Įkeltas failas viršija upload_max_filesize direktyvą faile php.ini!';
+$_['error_upload_2']          = 'Įspėjimas: Įkeltas failas viršija MAX_FILE_SIZE direktyvą, kuri nurodyta HTML formoje!';
+$_['error_upload_3']          = 'Įspėjimas: Failas įkeltas nepilnai!';
+$_['error_upload_4']          = 'Įspėjimas: Failai neįkelti!';
+$_['error_upload_6']          = 'Įspėjimas: Nerastas laikinasis katalogas!';
+$_['error_upload_7']          = 'Įspėjimas: Nepavyko įrašyti failo!';
+$_['error_upload_8']          = 'Įspėjimas: Failo kėlimas sustabdytas dėl plėtinio!';
+$_['error_upload_999']        = 'Įspėjimas: Klaidos kodas nerastas!';
+?>
