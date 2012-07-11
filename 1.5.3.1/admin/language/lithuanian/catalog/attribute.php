49ac03a77c342ed5a05b28b9dd367812586b39cf
Andrejus <andrejuszl@gmail.com>
2012-07-11 03:00:21 +0300

    Pataisymas

--- 1.5.3.1/admin/language/lithuanian/catalog/attribute.php
+++ 1.5.3.1/admin/language/lithuanian/catalog/attribute.php
@@ -6,18 +6,18 @@ $_['heading_title']          = 'Atributai';
 $_['text_success']           = 'Pavyko: Jūs sėkmingai modifikavote atributus!';
 
 // Column
-$_['column_name']            = 'Atributo vardas';
+$_['column_name']            = 'Atributo pavadinimas';
 $_['column_attribute_group'] = 'Atributo grupė';
 $_['column_sort_order']      = 'Rūšiavimo eiliškumas';
 $_['column_action']          = 'Veiksmas';
 
 // Entry
-$_['entry_name']            = 'Atributo vardas:';
+$_['entry_name']            = 'Atributo pavadinimas:';
 $_['entry_attribute_group'] = 'Atributo grupė:';
 $_['entry_sort_order']      = 'Rūšiavimo eiliškumas:';
 
 // Error
 $_['error_permission']      = 'Įspėjimas: Jūs neturite teisių modifikuoti atributus!';
-$_['error_name']            = 'Atributo vardas turi būti nuo 3 iki 64 simbolių!';
+$_['error_name']            = 'Atributo pavadinimas turi būti nuo 3 iki 64 simbolių!';
 $_['error_product']         = 'Įspėjimas: Šis atributas negali būti ištrintas, nes yra priskirtas %s produktams!';
 ?> 
