40f0ea9ab0153f360c0a2c4cb3fa03bc3a0d06fe
Jonuxas <jonuxas477@gmail.com>
2012-07-20 16:10:26 +0300

    Update Opencart vertimas v1.5.3.1/catalog/language/lithuanian/payment/authorizenet_aim.php

--- Opencart vertimas v1.5.3.1/catalog/language/lithuanian/payment/authorizenet_aim.php
+++ Opencart vertimas v1.5.3.1/catalog/language/lithuanian/payment/authorizenet_aim.php
@@ -1,12 +1,12 @@
 <?php
 // Text
-$_['text_title']           = 'Credit Card / Debit Card (Authorize.Net)';
-$_['text_credit_card']     = 'Credit Card Details';
-$_['text_wait']            = 'Please wait!';
+$_['text_title']           = 'Kreditinė kortelė / Debetinė kortelė (Authorize.Net)';
+$_['text_credit_card']     = 'Debetinės kortelės detalės';
+$_['text_wait']            = 'Prašome palaukti!';
 
 // Entry
-$_['entry_cc_owner']       = 'Card Owner:';
-$_['entry_cc_number']      = 'Card Number:';
-$_['entry_cc_expire_date'] = 'Card Expiry Date:';
-$_['entry_cc_cvv2']        = 'Card Security Code (CVV2):';
+$_['entry_cc_owner']       = 'Kortelės savininkas:';
+$_['entry_cc_number']      = 'Kortelės numeris:';
+$_['entry_cc_expire_date'] = 'Kortelės galiojimo pabaigos data:';
+$_['entry_cc_cvv2']        = 'Kortelės apsaugos kodas (CVV2):';
 ?>
